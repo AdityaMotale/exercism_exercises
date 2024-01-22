af5c8651cdd0f0a2bb33a0cd07a5e083b437c32b
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-01-22 10:23:50 +0530

    completed 15

---
+++ 15_high_scores.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        HighScores { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().cloned()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().cloned()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut top_three_vec = self.scores.to_vec();
+        top_three_vec.sort_unstable_by(|a, b| b.cmp(a));
+        top_three_vec.truncate(3);
+
+        top_three_vec
+    }
+}
