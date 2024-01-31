b7ea89295a5817701af3206403f7a7cf3b3bc8cb
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-01-31 23:06:20 +0530

    completed 23

--- 23_clock.rs
+++ 23_clock.rs
@@ -1,78 +1,31 @@
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    mins: i32,
+    min: i32,
 }
 
 impl Clock {
+    // Constructs a new Clock instance
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let hrs = Self::handle_hrs_overflow(hours);
-        let mins = Self::handle_minutes_overflow(minutes);
+        let total_minutes = hours * 60 + minutes;
 
         Clock {
-            mins: (hrs * 60) + mins,
+            min: Self::custom_rem_euclid(total_minutes, 1440), // 1440 minutes in a day
         }
     }
 
+    // Adds minutes to the clock
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock {
-            mins: self.mins + minutes,
-        }
+        Self::new(0, self.min + minutes)
     }
 
     pub fn to_string(&self) -> String {
-        let hrs = Self::get_time_in_format(Self::handle_hrs_overflow(self.mins / 60));
-        let minutes = Self::get_time_in_format(self.mins % 60);
+        let hrs = Self::format_time_object(self.min / 60);
+        let minutes = Self::format_time_object(self.min % 60);
 
         String::from(format!("{}:{}", hrs, minutes))
     }
 
-    fn handle_hrs_overflow(mut hr: i32) -> i32 {
-        if hr < 0 {
-            if hr < -24 {
-                hr = hr % -24;
-            }
-
-            return 24 + hr;
-        }
-
-        if hr < 24 {
-            return hr;
-        }
-
-        hr = hr % 24;
-
-        if hr == 24 {
-            hr = 0;
-        }
-
-        hr
-    }
-
-    fn handle_minutes_overflow(mut min: i32) -> i32 {
-        // 24 * 60, maximum minutes in a day
-        let MINUTES = 1440;
-
-        if min < 0 {
-            if min < -1440 {
-                min = min % -1440;
-                println!("{}", min);
-
-                if min < -1440 {
-                    return min + MINUTES;
-                }
-
-                return min;
-            }
-
-            return min;
-        }
-
-        min = min % MINUTES;
-
-        min
-    }
-
-    fn get_time_in_format(time: i32) -> String {
+    fn format_time_object(time: i32) -> String {
         let mut formatted_string = String::from(&time.to_string());
 
         if formatted_string.len() == 1 {
@@ -80,5 +33,16 @@ impl Clock {
         }
 
         formatted_string
+    }
+
+    // Computes the Euclidean remainder
+    fn custom_rem_euclid(dividend: i32, divisor: i32) -> i32 {
+        let remainder = dividend % divisor;
+
+        if remainder < 0 {
+            remainder + divisor
+        } else {
+            remainder
+        }
     }
 }
